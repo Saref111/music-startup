afab897d9fd78c5af6ac30ae08c9effc69130a18
Saref111 <saref012@gmail.com>
2020-09-08 13:09:12 +0300

    menu

--- source/js/script.js
+++ source/js/script.js
@@ -1,40 +1,22 @@
 'use strict';
 var sliderContainerWidth = document.querySelector('.slider__list-wrapper').clientWidth;
-var sliderListWidth = document.querySelector('.slider__list').clientWidth;
+var sliderListElement = document.querySelector('.slider__list');
+var slide = sliderListElement.querySelector('li');
 var mainNavElement = document.querySelector('.main-nav');
 var toggleButtonElement = document.querySelector('.main-nav__toggle');
 var mainNavListElement = document.querySelector('.main-nav__list');
+var sliderListWidth = sliderListElement.clientWidth;
 
-function toggleBurger () {
-    mainNavElement.classList.toggle('main-nav--open');
-    toggleButtonElement.classList.toggle('main-nav__toggle--close');
-    mainNavListElement.classList.toggle('main-nav__list--hidden-mobile');
-};
-toggleBurger();
-
-toggleButtonElement.addEventListener('click', toggleBurger);
-
-function getSlidesPerView(width) {
-    if (width <= 1425 && width > 1400) {
-        return 4;
-    } else if (width <= 1400 && width > 1294) {
-        return 3.9;
-    } else if (width <= 1294 && width > 1260) {
-        return 3.8;
-    } else if (width <= 1260 && width > 1224) {
-        return 3.7;
-    } else if (width <= 1224 && width > 1194) {
-        return 3.6;
-    } else if (width <= 1194 && width > 1163) {
-        return 3.5;
-    } else if (width <= 1163 && width > 1128) {
-        return 3.4;
-    } else if (width < 1000) {
-        return 1.5;
-    }
-}
-
-
+(function () {
+    function toggleBurger () {
+        mainNavElement.classList.toggle('main-nav--open');
+        toggleButtonElement.classList.toggle('main-nav__toggle--close');
+        mainNavListElement.classList.toggle('main-nav__list--hidden-mobile');
+    };
+    
+    toggleBurger();
+    toggleButtonElement.addEventListener('click', toggleBurger);
+})();
 
 var swiperMain = new Swiper('.main-slider', {
     navigation: {
@@ -43,11 +25,9 @@ var swiperMain = new Swiper('.main-slider', {
     },
 });
 
-if (sliderListWidth > sliderContainerWidth) {
-    console.log(sliderContainerWidth, getSlidesPerView(sliderContainerWidth));
-    
+if (sliderListWidth > sliderContainerWidth) {    
     var swiperNew = new Swiper('.slider__list-wrapper', {
-            slidesPerView: getSlidesPerView(sliderContainerWidth),
+            slidesPerView: (sliderContainerWidth / (slide.clientWidth + 72)) + 0.2 ,
             freeMode: true
     });
 }
