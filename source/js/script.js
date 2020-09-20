5046271224525d6e50a80a7ff73a434ce373a25c
Saref111 <saref012@gmail.com>
2020-09-20 12:16:26 +0300

    added swiper for tracklist

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,16 @@
 'use strict';
-var sliderContainerWidth = document.querySelector('.slider__list-wrapper').clientWidth;
-var sliderListElement = document.querySelector('.slider__list');
-var slide = sliderListElement.querySelector('li');
-var mainNavElement = document.querySelector('.main-nav');
-var toggleButtonElement = document.querySelector('.main-nav__toggle');
-var mainNavListElement = document.querySelector('.main-nav__list');
-var sliderListWidth = sliderListElement.clientWidth;
 
 (function () {
+    var sliderContainerWidth = document.querySelector('.slider__list-wrapper').clientWidth;
+    var sliderListElement = document.querySelector('.slider__list');
+    var slide = sliderListElement.querySelector('li');
+    var mainNavElement = document.querySelector('.main-nav');
+    var toggleButtonElement = document.querySelector('.main-nav__toggle');
+    var mainNavListElement = document.querySelector('.main-nav__list');
+    var sliderListWidth = sliderListElement.clientWidth;
+    var trackList =  document.querySelector('.slider__tracklist-wrapper');
+
+
     function toggleBurger () {
         mainNavElement.classList.toggle('main-nav--open');
         toggleButtonElement.classList.toggle('main-nav__toggle--close');
@@ -16,18 +19,27 @@ var sliderListWidth = sliderListElement.clientWidth;
     
     toggleBurger();
     toggleButtonElement.addEventListener('click', toggleBurger);
-})();
 
-var swiperMain = new Swiper('.main-slider', {
-    navigation: {
-        nextEl: '.main-slider__btn--next',
-        prevEl: '.main-slider__btn--prev',
-    },
-});
 
-if (sliderListWidth > sliderContainerWidth) {    
-    var swiperNew = new Swiper('.slider__list-wrapper', {
+
+    var swiperMain = new Swiper('.main-slider', {
+        navigation: {
+            nextEl: '.main-slider__btn--next',
+            prevEl: '.main-slider__btn--prev',
+        },
+    });
+    
+    if (sliderListWidth > sliderContainerWidth) {    
+        var swiperNew = new Swiper('.slider__list-wrapper', {
             slidesPerView: (sliderContainerWidth / (slide.clientWidth + 72)) + 0.2 ,
             freeMode: true
+        });
+    }
+    
+    var trackListSwiper = new Swiper('.slider__tracklist-wrapper', {
+        freeMode: true,
+        direction: 'vertical',
+        slidesPerView: 5,
+        spaceBetween: 28
     });
-}
+})();
